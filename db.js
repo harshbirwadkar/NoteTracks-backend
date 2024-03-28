a7097f846d166f15f47012eba5ac89cbd4765dd2
harshbirwadkar <harshbirwadkar3@gmail.com>
2024-03-29 01:47:41 +0530

    changes

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 // const mongoURI = "mongodb://localhost:27017/inotebook"
-const mongoURI = `mongodb+srv://${process.env.NOTETRACK_APP_DATABASE_ID}:${process.env.NOTETRACK_APP_DATABASE_PASS}@cluster0.asga3ec.mongodb.net/inotebook`
+// const mongoURI = `mongodb+srv://harshbirwadkar3:<password>@cluster0.asga3ec.mongodb.net/`
+const mongoURI = process.env.NOTETRACK_APP_DATABASE
+// const mongoURI = `mongodb+srv://${process.env.NOTETRACK_APP_DATABASE_ID}:${process.env.NOTETRACK_APP_DATABASE_PASS}@cluster0.asga3ec.mongodb.net/inotebook`
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI)
--- index.js
+++ index.js
@@ -8,7 +8,8 @@ connectToMongo()
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(cors())
+// app.use(cors())
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.use(express.json())
 app.use('/api/auth' , require('./routes/auth'))
